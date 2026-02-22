3935b2c0573c20882c5feaa3a8df7f93c08851bd
Maverick Seneris <maverickseneris@gmail.com>
2026-02-22 13:25:06 +0800

    initial commit

---
+++ client/src/app/page.tsx
@@ -0,0 +1,24 @@
+async function loader(){
+  const path = "api/home-page";
+  const BASE_URL = "http://localhost:1338";
+  const url = new URL(path, BASE_URL);
+  
+  const response = await fetch(url.href);
+  const data = await response.json();
+  console.log(data)
+
+  return {... data.data};
+}
+
+
+export default async function HomeRoute() {
+  const data = await loader();
+  console.log(data);
+  
+  return (
+    <div>
+      <h1>{data.title}</h1>
+      <p>{data.description}</p>
+    </div>
+  );
+}
